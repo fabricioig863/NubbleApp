5e6d9a6b48f94d2088c07dd702be56d716e1b4b1
Fabricio Ignacio <fabricioig863@gmail.com.br>
2023-12-23 23:45:02 -0300

    refactor: Remove console.log

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -5,7 +5,6 @@ import {Post} from './postType';
 async function getList(): Promise<Post[]> {
   const postPageAPI = await postAPi.getList();
 
-  console.log(postPageAPI);
   return postPageAPI.data.map(postAdapter.toPost);
 }
 

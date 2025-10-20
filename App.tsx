e0974a675d0a2568eeaff000bec58d6a905ad37c
fabricioig863 <fabricioig863@gmail.com>
2025-10-19 22:02:05 -0300

    feat: first commit nubble App

--- App.tsx
+++ App.tsx
@@ -1,45 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { ThemeProvider } from '@shopify/restyle';
+import { View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Box } from './src/components/Box/Box';
+import { Button } from './src/components/Button/Button';
+import { Text } from './src/components/Text/Text';
+import { theme } from './src/theme/theme';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={{ paddingHorizontal: 24 }}>
+            <Text preset="headingLarge" italic>
+              CoffStack
+            </Text>
+            <Box marginBottom="s24">
+              <Button title="Entrar" />
+            </Box>
+            <Button loading title="Carregando..." />
+
+            <Box mt="s20">
+              <Button
+                title="Entrar"
+                marginBottom="s12"
+                backgroundColor="grayBlack"
+              />
+            </Box>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,36 @@
+import {
+  backgroundColor,
+  BackgroundColorProps,
+  border,
+  BorderProps,
+  createBox,
+  createRestyleComponent,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import React from 'react';
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { Theme } from '../../theme/theme';
+
+export const Box = createBox<Theme>();
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  LayoutProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  BorderProps<Theme> &
+  TouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, spacingShorthand, layout, border],
+  TouchableOpacity,
+);

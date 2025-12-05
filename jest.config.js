77717cc3d4dd0d18589e17aab45b73c916568608
fabricioig863 <fabricioig863@gmail.com>
2025-12-05 09:12:08 -0300

    config jestSetup and create tests hooks

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: 'react-native',
-  collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+  ],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation)/)',
--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {render, RenderOptions} from '@testing-library/react-native';
 
 import {theme} from '@theme';
 
-const AllTheProvider = ({children}: {children: React.ReactNode}) => {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      gcTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      gcTime: Infinity,
+    },
+  },
+});
+
+export const AllTheProvider = ({children}: {children: React.ReactNode}) => {
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>{children}</NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>{children}</NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
